780f7aa5ff65b3f53d8a1f946a20b7df327762b4
chellyzzz <2795707251@qq.com>
2026-05-08 13:48:48 +0200

    refactor: move cop issue readiness into queue regs

--- vsrc/idu/idu_cop_regs.v
+++ vsrc/idu/idu_cop_regs.v
@@ -3,12 +3,14 @@ module hcpu_idu_cop_regs (
     input               reset,
     input               i_capture,
     input               i_clear,
+    input               i_backend_busy,
     input      [31:0]   i_pc,
     input      [31:0]   i_src1,
     input      [31:0]   i_src2,
     input      [4:0]    i_rd,
     input               i_wen,
     output reg          o_inflight,
+    output              o_issue_ready,
     output reg [31:0]   o_pc,
     output     [31:0]   o_active_src1,
     output     [31:0]   o_active_src2,
@@ -21,6 +23,7 @@ reg [31:0] src2;
 
 assign o_active_src1 = o_inflight ? src1 : i_src1;
 assign o_active_src2 = o_inflight ? src2 : i_src2;
+assign o_issue_ready = !o_inflight && !i_backend_busy;
 
 always @(posedge clock or posedge reset) begin
     if (reset) begin
